9d32da7a9f6caf73afcd18021246fa8426b04c2d
cristianooliveiramorales@gmail.com <cristianooliveiramorales@gmail.com>
2024-09-01 23:35:25 -0300

    Adição do componente de carrossel de livros

---
+++ resources/js/Components/Carrossel.jsx
@@ -0,0 +1,50 @@
+
+import { NavigateBefore, NavigateNext } from '@mui/icons-material';
+import { useEffect, useState } from 'react';
+
+export default function Carrossel() {
+    const [livros, setLivros] = useState([]);
+    const [carregando, setCarregando] = useState(true);
+    const [fimDaLista, setFimDaLista] = useState(false);
+    const [page, setPage] = useState(1);
+    const larguraMaximaDoLivro = 200
+    const max = Math.floor(window.innerWidth / larguraMaximaDoLivro);
+
+    const handlePrevious = () => {
+        const novaPagina = page - 1;
+        setPage(novaPagina >= 1 ? novaPagina : 1);
+    };
+    const handleNext = () => setPage(fimDaLista ? page : page + 1);
+
+    useEffect(() => {
+        fetch(`https://potterapi-fedeperin.vercel.app/pt/books?max=${max}&page=${page}`)
+            .then(r => r.json())
+            .then(r => {
+                setLivros(r)
+                if (r.length) setFimDaLista(false);
+                else setFimDaLista(true);
+
+            })
+            .catch(() => setCarregando(false))
+    }, [page]);
+    
+    useEffect(() => {
+        if (!livros.length) return;
+
+        setCarregando(false);
+    }, [livros]);
+   
+    return <div className="card">
+        {page > 1 && <NavigateBefore className='card__navigate card__navigate--before' fontSize='large' onClick={handlePrevious}/>}
+        {!fimDaLista && <NavigateNext className='card__navigate card__navigate--next' fontSize='large' onClick={handleNext} />}
+
+        {carregando
+            ? "Carregando..."
+            : livros.length
+                ? livros.map(livro => <div className='livro' key={livro.index}>
+                    <img src={livro.cover} />
+                    {livro.title}
+                </div>)
+                : "Nenhum livro encontrado"}
+    </div>
+}
---
+++ resources/js/Pages/Livros/Index.jsx
@@ -0,0 +1,18 @@
+import Carrossel from '@/Components/Carrossel';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import { Head } from '@inertiajs/react';
+
+export default function Edit({ auth }) {
+    return (
+        <AuthenticatedLayout
+            user={auth.user}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Livros</h2>}
+        >
+            <Head title="Livros" />
+
+            <div className="py-12">
+                <Carrossel />
+            </div>
+        </AuthenticatedLayout>
+    );
+}
